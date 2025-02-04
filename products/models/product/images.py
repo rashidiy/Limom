6af5dacfa453954c563031429212dc4db6dedbec
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-02-04 15:23:44 +0500

    Image model translated into three languages

--- products/models/product/images.py
+++ products/models/product/images.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django_resized import ResizedImageField
-
+from django.utils.translation import gettext_lazy as _
 
 class ProductImage(models.Model):
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name='images')
-    image = ResizedImageField(size=[300, 300], crop=['middle', 'center'], upload_to='products/images')
+    image = ResizedImageField(size=[300, 300], crop=['middle', 'center'],  upload_to='products/images')
+
+    class Meta:
+        verbose_name = _('Product Image')
+        verbose_name_plural = _('Product Images')
