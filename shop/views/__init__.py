76d91c719d9c049994f9e94002931701f96ebf12
hojiakbar077 <hojiakbarhabibullayev664@gmail.com>
2025-02-04 19:25:55 +0500

    fix __init__.py

--- shop/views/__init__.py
+++ shop/views/__init__.py
@@ -1,1 +1,13 @@
 from .homepage import HomePageTemplateView
+from .shop import (ShopPageTemplateView, SingleProductTabStyleLeftTemplateView, SingleProductGroupTemplateView,
+                   SingleProductNormalTemplateView, SingleProductTemplateView, SingleProductSaleTemplateView,
+                   SingleProductAffinityTemplateView, Shop3PageTemplateView, Shop4PageTemplateView,
+                   ShopRightSidebarPageTemplateView, ShopListPageTemplateView,ShopListPageTemplateView,
+                    ShopListLeftSidebarPageTemplateView,ShopListRightSidebarPageTemplateView,
+                    SingleProductGalleryLeftTemplateView,SingleProductCarouselTemplateView,SingleProductGalleryRightTemplateView,
+                    SingleProductTabStyleTopTemplateView,SingleProductTabStyleRightTemplateView,ShoppingCardTemplateView
+                    )
+from .wishlist import WishListView
+from .login_register import LoginRegisterPageTemplateView
+from .checkout import CheckOutPageTemplateView
+from .compare import CompareTemplateView
