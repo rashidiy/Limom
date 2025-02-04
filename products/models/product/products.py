e7c58a430174b25193114b01526ae456af6c3a7c
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-02-04 15:24:05 +0500

    Product model translated into three languages

--- products/models/product/products.py
+++ products/models/product/products.py
@@ -1,16 +1,21 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField(validators=[MinValueValidator(0)])
+    title = models.CharField(_('Title'), max_length=100)
+    price = models.DecimalField(_('Price'), max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(_('Quantity'), validators=[MinValueValidator(0)])
     category = models.ForeignKey('products.Category', on_delete=models.CASCADE, related_name='products')
     seller = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='products')
 
-    short_description = models.CharField(max_length=2048)
-    long_description = models.CharField(max_length=2048)  # todo: @thenodirjon CKeditorni qo'shish
+    short_description = models.CharField(_('Short description'), max_length=2048)
+    long_description = models.CharField(_('Long description'), max_length=2048)  # todo: @thenodirjon CKeditorni qo'shish
+
+    class Meta:
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
 
     @property
     def rating(self):
