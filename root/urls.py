62790f54cee858c4e08b02ba5ac08a4b86fc7d24
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-02-04 15:28:42 +0500

    i18n translation module urls configured

--- root/urls.py
+++ root/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('', include(('shop.urls', 'shop'), namespace='shop')),
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-]
+    path('', include(('shop.urls', 'shop'), namespace='shop')),
+)+i18n_patterns(
+    path("i18n/", include("django.conf.urls.i18n")),
+)
