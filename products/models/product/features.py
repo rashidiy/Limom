5860e11aaeaf131f035cbc4acc16cc7055283691
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-02-04 15:23:09 +0500

    Feature model translated into three languages

--- products/models/product/features.py
+++ products/models/product/features.py
@@ -1,7 +1,11 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 
 class ProductDimension(models.Model):
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
-    width = models.FloatField(blank=True)
-    height = models.FloatField(blank=True)
+    width = models.FloatField(_('Width'),blank=True)
+    height = models.FloatField(_('Height'),blank=True)
+
+    class Meta:
+        verbose_name = _('Product Dimension')
+        verbose_name_plural = _('Product Dimensions')
