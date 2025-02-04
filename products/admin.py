85e60f08963ada049ac4f123f1df58c554db0de7
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-02-04 15:34:26 +0500

    Models transferred to admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
+from products.models import (Product, Category,
+                             ProductImage, ProductDimension,
+                             ProductReview, ProductDiscount)
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProductDimension)
+class ProductDimensionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProductReview)
+class ProductReviewAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProductDiscount)
+class ProductDiscountAdmin(admin.ModelAdmin):
+    pass
