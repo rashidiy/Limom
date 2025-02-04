e325440c2caa0f0636c1ac49b4fa82f8ced1e84c
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-02-04 15:22:17 +0500

    Discount model translated into three languages

--- products/models/product/discounts.py
+++ products/models/product/discounts.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django_resized import ResizedImageField
-
+from django.utils.translation import gettext_lazy as _
 
 class ProductDiscount(models.Model):
-    banner = ResizedImageField()
+    banner = ResizedImageField(_('Banner'))
+    class Meta:
+        verbose_name = _('Product Discount')
+        verbose_name_plural = _('Product Discounts')
