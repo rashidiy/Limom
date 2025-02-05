adb8c3af19f6807a6dcf14e8ad4c5c53cee626b8
user <akramibodullayev0011@gmail.com>
2025-02-05 21:49:31 +0500

    my acount qismi tugirlandi

--- shop/views/login_register.py
+++ shop/views/login_register.py
@@ -6,53 +6,63 @@ from shop.forms import RegistrationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from shop.forms import UserChangeForm
+from django.contrib.auth.models import User
 
 
 class RegisterView(View):
     def get(self, request):
         form = RegistrationForm()
-        return render(request, 'login-register/login-register.html', {'register_form': form})
+        return render(request, 'login-register/login-register.html', {'form': form})
 
     def post(self, request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            user.backend = 'shop.authentication_backends.EmailBackend'
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password1"])
+            user.save()
             login(request, user)
-            return redirect('/')
-        return render(request, 'login-register/login-register.html', {'register_form': form})
+            return redirect('shop:home')
+        return render(request, 'login-register/login-register.html', {'form': form})
 
 
 class LoginView(View):
     def get(self, request):
         form = AuthenticationForm()
-        return render(request, 'login-register/login-register.html', {'login_form': form})
+        return render(request, 'login-register/login-register.html', {'form': form})
 
     def post(self, request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            user = form.get_user()
-            user.backend = 'shop.authentication_backends.EmailBackend'
-            login(request, user)
-            return redirect('/')
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('shop:home')
         messages.error(request, "Invalid email or password!")
-        return render(request, 'login-register/login-register.html', {'login_form': form})
+        return render(request, 'login-register/login-register.html', {'form': form})
 
 
 class LogoutView(View):
     def get(self, request):
         logout(request)
-        request.session.flush()
         return redirect('shop:home')
 
-@login_required(login_url='shop:login')
+
+from django.contrib import messages
+
+
+
+
+@login_required(login_url='login')
 def edit_profile(request):
     user = request.user
     if request.method == 'POST':
         form = UserChangeForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('shop:home')
     else:
         form = UserChangeForm(instance=user)
     return render(request, 'profile/edit_profile.html', {'form': form})
