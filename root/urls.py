e90c5abd0f2df1397dd99e5557928854749d94a4
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-02-05 20:13:33 +0500

    Updeted product urls

--- root/urls.py
+++ root/urls.py
@@ -27,3 +27,5 @@ urlpatterns = i18n_patterns(
     path("i18n/", include("django.conf.urls.i18n")),
 
 )
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
