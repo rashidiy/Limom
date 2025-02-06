17383321b8b2ec5d73ed92d13cbf4141f8583528
rustam <odiloffr@gmail.com>
2025-02-06 15:34:54 +0500

    Updated settings

--- root/settings.py
+++ root/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     # packege
     'django_ckeditor_5',
 
@@ -86,7 +87,7 @@ ROOT_URLCONF = 'root.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
