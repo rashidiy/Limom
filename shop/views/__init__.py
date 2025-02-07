00a05b7dbece5eefeb5a716a385a0c184f5ac228
rustam <odiloffr@gmail.com>
2025-02-07 21:59:35 +0500

    Changed init

--- shop/views/__init__.py
+++ shop/views/__init__.py
@@ -1,48 +1,61 @@
-# from .about_us import AboutUsTemplateView
-# from .accessories import AccessoriesTemplateView
-from .blog_list import (BlogPageTemplateView,
-                        Blog3PageView,
-                        Blog4PageView,
-                        Blog5PageView,
-                        BlogListTemplateView,
-                        Blog6PageView,
-                        Blog7PageView,
-                        Blog8PageView,
-                        Blog9PageView,
-                        BlogAudiView,
-                        BlogVideoView,
-                        BlogGalaryView)
-from .checkout import *
+from .blog_list import (
+    BlogPageTemplateView,
+    Blog3PageView,
+    Blog4PageView,
+    Blog5PageView,
+    BlogListTemplateView,
+    Blog6PageView,
+    Blog7PageView,
+    Blog8PageView,
+    Blog9PageView,
+    BlogAudiView,
+    BlogVideoView,
+    BlogGalaryView
+)
+
 from .checkout import CheckOutPageTemplateView
-from .compare import *
-from .search import *
+
 from .compare import CompareTemplateView
-# from .contact import ContactTemplateView
-from .homepage import *
+
 from .homepage import HomePageTemplateView
+
 from .login_register import *
+
 from .page_log_reg import (
     ContactView,
     AboutView,
     FaqView,
-    ErrorView)
-from .page_log_reg import ContactView, AboutView, FaqView, ErrorView
-from .shop import *
-from .shop import (ShopPageTemplateView, SingleProductTabStyleLeftTemplateView, SingleProductGroupTemplateView,
-                   SingleProductNormalTemplateView, SingleProductTemplateView, SingleProductSaleTemplateView,
-                   SingleProductAffinityTemplateView, Shop3PageTemplateView, Shop4PageTemplateView,
-                   ShopRightSidebarPageTemplateView, ShopListPageTemplateView, ShopListPageTemplateView,
-                   ShopListLeftSidebarPageTemplateView, ShopListRightSidebarPageTemplateView,
-                   SingleProductGalleryLeftTemplateView, SingleProductCarouselTemplateView,
-                   SingleProductGalleryRightTemplateView,
-                   SingleProductTabStyleTopTemplateView, SingleProductTabStyleRightTemplateView,
-                   ShoppingCardTemplateView
-                   )
-# from .smartwatch import SmartwatchTemplateView
-from .wishlist import WishListView
-# from .search import search_products
+    ErrorView
+)
 
-from .checkout import CheckOutPageTemplateView
-from .compare import CompareTemplateView
+from .shop import (
+    ShopPageTemplateView,
+    SingleProductTabStyleLeftTemplateView,
+    SingleProductGroupTemplateView,
+    SingleProductNormalTemplateView,
+    SingleProductTemplateView,
+    SingleProductSaleTemplateView,
+    SingleProductAffinityTemplateView,
+    Shop3PageTemplateView,
+    Shop4PageTemplateView,
+    ShopRightSidebarPageTemplateView,
+    ShopListPageTemplateView,
+    ShopListLeftSidebarPageTemplateView,
+    ShopListRightSidebarPageTemplateView,
+    SingleProductGalleryLeftTemplateView,
+    SingleProductCarouselTemplateView,
+    SingleProductGalleryRightTemplateView,
+    SingleProductTabStyleTopTemplateView,
+    SingleProductTabStyleRightTemplateView,
+    ShoppingCardTemplateView,
+)
+
+from shop.views.about_us import AboutUsTemplateView
+from shop.views.contact import ContactTemplateView
+from shop.views.accessories import AccessoriesTemplateView
+from shop.views.smartwatch import SmartwatchTemplateView
+from shop.views.search import SearchProductsView
+
+from .wishlist import WishListView
 
 from .error import custom_404_view
