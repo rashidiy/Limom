0259ab8993f49fcec93b54ddc1d4cd890c855872
user <akramibodullayev0011@gmail.com>
2025-02-07 17:22:14 +0500

    hatolar tugirlandi ammo about us va contact smartwatch accesuares   yuq

--- root/urls.py
+++ root/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
