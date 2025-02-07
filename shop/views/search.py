6155305bc26492cbba6599d8e795700fdca3f4f0
rustam <odiloffr@gmail.com>
2025-02-07 22:02:19 +0500

    Updated search view

--- shop/views/search.py
+++ shop/views/search.py
@@ -1,24 +1,28 @@
+from django.views.generic import ListView
 from django.db.models import Q
-from django.shortcuts import render
-from products.models import Product, categories
-
-def search_products(request):
-    query = request.GET.get('q', '').strip()
-    products = Product.objects.all()
-
-    if query:
-        products = products.filter(
-            Q(title__icontains=query) |
-            Q(short_description__icontains=query) |
-            Q(long_description__icontains=query)|
-            Q(category__name__icontains=query)
-        )
+from products.models import Product, Category
 
 
-    print(f"Natijalar soni: {products.count()}")
-    for product in products:
-        print(product.title)  #
+class SearchProductsView(ListView):
+    model = Product
+    template_name = 'search-result.html'
+    context_object_name = 'products'
 
-    context = {'products': products, 'query': query}
-    return render(request, 'search-result.html', context)
+    def get_queryset(self):
+        query = self.request.GET.get('q', '').strip()
+        category = self.request.GET.get('category', 'all')
 
+        products = Product.objects.all()
+
+        if category != 'all':
+            products = products.filter(category__name__iexact=category)
+
+        if query:
+            products = products.filter(
+                Q(title__icontains=query) |
+                Q(short_description__icontains=query) |
+                Q(long_description__icontains=query) |
+                Q(category__name__icontains=query)
+            )
+
+        return products
